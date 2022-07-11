14d4725e35d8c9109292a343f5ed6ea1986c6530
klausmonga <monga.wakowa@gmail.com>
2022-07-11 16:37:04 +0200

    init

---
+++ bi_website_add_product/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': "Website Easy Add to Cart",
+    'version': '14.0.0.1',
+    'category': 'eCommerce',
+    'summary': """This Module is used for easy to Add product in cart""" ,
+    'description': 'This Module is used to Add product in cart, easy to Add product in cart, dynamic add to cart option, dynamic product add to cart option, easy add to cart option on shop',
+    'author': 'BrowseInfo',
+    'website' : 'https://www.browseinfo.in',
+    'depends': ['base','website','sale','website_sale'],
+    'data'   : [
+                'views/template.xml',
+    		],
+	'installable': True,
+	'application': True,
+    'auto_install': False,
+    'live_test_url':'https://youtu.be/-mOJOIblJnI',
+    "images":["static/description/Banner.png"],
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
